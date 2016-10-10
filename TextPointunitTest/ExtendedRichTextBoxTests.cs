6c5774aa9a040ed135edb40250ef10836c545a7f
naxe <christer.stuxberg@gmail.com>
2016-10-10 13:28:38 +0200

    started making tests for the extended richtextbox class

---
+++ TextPointunitTest/ExtendedRichTextBoxTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextPoint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace TextPointunitTest
+{
+    [TestClass()]
+    public class ExtendedRichTextBoxTests
+    {
+        RichTextBox rtb;
+
+        [TestMethod()]
+        public void AppendWithColorTest()
+        {
+            rtb = new RichTextBox();
+            ExtendedRichTextBox ertb = new ExtendedRichTextBox(rtb);
+            Assert.IsTrue(rtb.Text.Length == 0);
+            ertb.AppendWithColor("test", Color.Blue);
+            rtb.SelectAll();
+            Assert.AreEqual(Color.Blue, rtb.SelectionBackColor);
+            Assert.IsTrue(rtb.Text.Contains("test"));
+        }
+
+        [TestMethod()]
+        public void SameSizeSelectionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCurrentSizeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ChangeFormatTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BoldTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ItalicTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UnderlineTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
