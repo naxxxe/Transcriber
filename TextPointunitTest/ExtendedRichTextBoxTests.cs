889cf24a71efb1ecd7df8058f029729e73003777
naxe <christer.stuxberg@gmail.com>
2016-10-10 14:10:50 +0200

    Did some more tests

--- TextPointunitTest/ExtendedRichTextBoxTests.cs
+++ TextPointunitTest/ExtendedRichTextBoxTests.cs
@@ -14,29 +14,61 @@ namespace TextPointunitTest
     public class ExtendedRichTextBoxTests
     {
         RichTextBox rtb;
+        ExtendedRichTextBox ertb;
 
         [TestMethod()]
         public void AppendWithColorTest()
         {
             rtb = new RichTextBox();
-            ExtendedRichTextBox ertb = new ExtendedRichTextBox(rtb);
+            ertb = new ExtendedRichTextBox(rtb);
             Assert.IsTrue(rtb.Text.Length == 0);
+            rtb.BackColor = Color.White;
             ertb.AppendWithColor("test", Color.Blue);
             rtb.SelectAll();
             Assert.AreEqual(Color.Blue, rtb.SelectionBackColor);
+            Assert.AreNotEqual(rtb.BackColor, rtb.SelectionBackColor);
             Assert.IsTrue(rtb.Text.Contains("test"));
         }
 
         [TestMethod()]
-        public void SameSizeSelectionTest()
+        public void SameSizeSelectionTest_DifferentFonts_True()
         {
-            Assert.Fail();
+            rtb = new RichTextBox();
+            ertb = new ExtendedRichTextBox(rtb);
+            rtb.AppendText("testing1");
+            rtb.SelectionFont = new Font("Arial", rtb.SelectionFont.Size);
+            rtb.SelectedText = " testing2 ";
+            rtb.AppendText("testing 3");
+            rtb.SelectAll();
+            Assert.IsNull(rtb.SelectionFont);
+            Assert.IsTrue(ertb.SameSizeSelection());
         }
-
         [TestMethod()]
-        public void GetCurrentSizeTest()
+        public void SameSizeSelectionTest_DifferentFonts_False()
         {
-            Assert.Fail();
+            rtb = new RichTextBox();
+            ertb = new ExtendedRichTextBox(rtb);
+            rtb.AppendText("testing1");
+            rtb.SelectionFont = new Font("Arial",28);
+            rtb.SelectedText = " testing2 ";
+            rtb.AppendText("testing 3");
+            rtb.SelectAll();
+            Assert.IsNull(rtb.SelectionFont);
+            Assert.IsFalse(ertb.SameSizeSelection());
+        }
+        [TestMethod]
+        public void SameSizeSelectionTest_SameFontDifferentSize_False()
+        {
+            rtb = new RichTextBox();
+            ertb = new ExtendedRichTextBox(rtb);
+            rtb.AppendText("testing1");
+            rtb.SelectionFont = new Font(rtb.SelectionFont.Name, 28);
+            rtb.SelectedText = " testing2 ";
+            rtb.AppendText("testing 3");
+            rtb.SelectAll();
+            Assert.IsNotNull(rtb.SelectionFont);
+            Assert.AreEqual(13, rtb.SelectionFont.Size);
+            Assert.IsFalse(ertb.SameSizeSelection());
         }
 
         [TestMethod()]
